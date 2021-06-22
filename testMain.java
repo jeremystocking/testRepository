e14215e4044468ea18acf6d2f78bb692b498a5a5
Jeremy Stocking <jeremystocking@u.boisestate.edu>
2021-06-22 12:14:32 -0600

    The Original Application

---
+++ testMain.java
@@ -0,0 +1,14 @@
+package testRepository;
+
+import java.util.Scanner;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Please Enter your name: ");
+		Scanner scan = new Scanner(System.in);
+		String userName = scan.next();
+		System.out.println("Thank you for joining us " + userName + "!");
+	}
+}
